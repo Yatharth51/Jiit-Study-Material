f414dab94a8042b947cffa19422091dfaca5336e
Yatharth51 <yatharth51@gmail.com>
2025-02-02 19:41:07 +0530

    updated for deployment

---
+++ src/pages/SemesterPages/Sem6/SubjectContent.tsx
@@ -0,0 +1,52 @@
+export const SubjectContent = [
+    {
+        url : "CareerManagement",
+        title : "Career Management"
+    },
+    {
+        url : "InformationTheory",
+        title : "Information Theory"
+    },
+    {
+        url : "SoftwareEngineering",
+        title : "Software Engineering",
+        pyqs :"https://drive.google.com/drive/folders/1Gtz32hU6dzdcIAlfUZ_3f5vGcXrHmJDM?usp=sharing",
+    },
+    {
+        url:"SoftwareEngineeringLab",
+        title : "Software Engineering Lab"},
+    {
+        url : "CNIOT",
+        title :"CNIOT",
+        pyqs : "https://drive.google.com/drive/folders/1SsX5tP8QRk5msc_tGgdyLMPlWhc-IztD?usp=sharing",
+        slides : "" ,
+        tutorials: ""
+    },
+    {
+        url : "CNIOTLab",
+        title : "CNIOT Lab",
+        tutorials:"",
+    },
+    {
+        url : "FDCC",
+        title : "Distributed and Cloud Computing"
+    },
+    {
+        url :"STAP",
+        title :"Sensor and Android Programming"
+    },
+    {
+        url:"BigDataIngestion",
+        title :"Big Data Ingestion",
+        pyqs: "https://drive.google.com/drive/folders/1H7yzFYJXX3WmkGuUvAvcLUEybFj9bGNh?usp=sharing"
+    },
+    {
+        url : "OpenSourceSoftwareDevelopment",
+        title : "Open Source Software Development" ,
+        pyqs : "https://drive.google.com/drive/folders/1xphNnOajWfBHOXrcFTsoMZ6BRp92N5rb?usp=sharing"
+    },{
+        url : "IntroductiontoLargeScaleDatabaseSystem",
+        title : "Introduction to Large Scale Database System" ,
+        pyqs : "https://drive.google.com/drive/folders/1xcM22x-WTLvUXNz05zoZESrmH7g9qRGB?usp=sharing"
+    }
+]
