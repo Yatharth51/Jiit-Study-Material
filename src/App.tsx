6571dcaeeb5cc6dcca05abd83e01c0779ccb46d9
Yatharth51 <yatharth51@gmail.com>
2025-02-02 19:58:30 +0530

    added vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import { HomePage } from './pages/HomePage'
 import { StudyMaterial } from "./pages/StudyMaterial";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
